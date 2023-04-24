ced8494ac7cc49cf1a74f3d7d29c8416a4ecf24d
Sil <sil.vannoort@student.hu.nl>
2023-04-24 23:59:02 +0200

    updated html pages

--- src/web/categories.js
+++ src/web/categories.js
@@ -1,30 +1,3 @@
-const categories = [
-    {
-        name: "Living Room",
-        image: "living_room.jpg"
-    },
-    {
-        name: "Bedroom",
-        image: "bedroom.jpg"
-    },
-    {
-        name: "Kitchen",
-        image: "kitchen.jpg"
-    },
-    // ... more categories
-];
-
-function loadCategories() {
-    const categoriesContainer = document.getElementById("categories");
-    categories.forEach(category => {
-        const categoryElement = document.createElement("div");
-        categoryElement.className = "category";
-        categoryElement.innerHTML = `
-            <img src="${category.image}" alt="${category.name}">
-            <h3>${category.name}</h3>
-        `;
-        categoriesContainer.appendChild(categoryElement);
-    });
+function redirectToCategory(categoryPage) {
+    window.location.href = categoryPage;
 }
-
-document.addEventListener("DOMContentLoaded", loadCategories);
