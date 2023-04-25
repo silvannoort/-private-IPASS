a4776e65828b03d138a3a2db5bf53c0a4179c640
Sil <sil.vannoort@student.hu.nl>
2023-04-25 06:15:32 +0200

    front end almost finished

--- src/web/categories.js
+++ src/web/categories.js
@@ -1,3 +1,9 @@
 function redirectToCategory(categoryPage) {
     window.location.href = categoryPage;
 }
+AOS.init({
+    duration: 1000, // animatieduur in milliseconden
+    delay: 100, // vertraging in milliseconden
+    once: true, // of de animatie slechts één keer of bij elke scroll moet worden uitgevoerd
+  });
+  
